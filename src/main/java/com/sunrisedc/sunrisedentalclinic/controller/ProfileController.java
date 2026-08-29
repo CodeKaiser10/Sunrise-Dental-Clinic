9ffae74211c41bd06cd279253381af8b9ac3f59a
Shahindu <shahindu0001@gmail.com>
2026-08-30 01:38:20 +0530

    Profile and View help and jsp files added

---
+++ src/main/java/com/sunrisedc/sunrisedentalclinic/controller/ProfileController.java
@@ -0,0 +1,20 @@
+package com.sunrisedc.sunrisedentalclinic.controller;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//shows logged-in user to their own details
+@WebServlet("/profile")
+public class ProfileController extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.getRequestDispatcher("WEB-INF/view/profile.jsp").forward(request, response);
+    }
+}
